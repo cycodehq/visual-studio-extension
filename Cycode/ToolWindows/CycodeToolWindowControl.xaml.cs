68a4da22b459e750ae28acf915756166cdfc0aa0
Ilya Siamionau <ilya.siamionau@cycode.com>
2024-06-04 17:22:30 +0200

    CM-36341 - Create plugin skeleton

---
+++ Cycode/ToolWindows/CycodeToolWindowControl.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Cycode
+{
+    public partial class CycodeToolWindowControl : UserControl
+    {
+        public CycodeToolWindowControl()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            VS.MessageBox.Show("Cycode", "Button clicked");
+        }
+    }
+}
