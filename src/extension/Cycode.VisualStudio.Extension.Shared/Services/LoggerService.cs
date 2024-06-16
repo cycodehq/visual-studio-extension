800e9a16246eb72353e2649686babbbe03fedaf0
Ilya Siamionau <ilya.siamionau@cycode.com>
2024-06-16 17:31:15 +0200

    fix day logging

--- src/extension/Cycode.VisualStudio.Extension.Shared/Services/LoggerService.cs
+++ src/extension/Cycode.VisualStudio.Extension.Shared/Services/LoggerService.cs
@@ -58,7 +58,7 @@ public class LoggerService : ILoggerService {
         }
 
         string logLevel = GetLogLevelPrefix(level);
-        formattedMessage = $"{DateTime.Now:MMM dd HH:mm:ss} [{logLevel}] {formattedMessage}{Environment.NewLine}";
+        formattedMessage = $"{DateTime.Now:ddd dd HH:mm:ss} [{logLevel}] {formattedMessage}{Environment.NewLine}";
 
         _pane?.OutputString(formattedMessage);
         Console.WriteLine(formattedMessage);
