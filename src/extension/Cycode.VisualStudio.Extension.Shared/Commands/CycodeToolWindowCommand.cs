ecdebe84ba78c113bc884deb1eb2c9596c72abd6
Ilya Siamionau <ilya.siamionau@cycode.com>
2024-06-26 17:22:01 +0200

    add tool window icon; add cycode to extensions top menu

--- src/extension/Cycode.VisualStudio.Extension.Shared/Commands/CycodeToolWindowCommand.cs
+++ src/extension/Cycode.VisualStudio.Extension.Shared/Commands/CycodeToolWindowCommand.cs
@@ -1,7 +1,14 @@
 ﻿namespace Cycode.VisualStudio.Extension.Shared;
 
-[Command(PackageIds.CycodeCommand)]
+[Command(PackageIds.CycodeToolWindowCommand)]
 internal sealed class CycodeToolWindowCommand : BaseCommand<CycodeToolWindowCommand> {
+    protected override Task ExecuteAsync(OleMenuCmdEventArgs e) {
+        return CycodeToolWindow.ShowAsync();
+    }
+}
+
+[Command(PackageIds.TopMenuCycodeCommand)]
+internal sealed class TopMenuCycodeCommand : BaseCommand<TopMenuCycodeCommand> {
     protected override Task ExecuteAsync(OleMenuCmdEventArgs e) {
         return CycodeToolWindow.ShowAsync();
     }
