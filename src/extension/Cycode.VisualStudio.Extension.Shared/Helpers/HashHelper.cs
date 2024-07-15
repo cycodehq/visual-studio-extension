093315ea8d109fb970d6e41261fc7feaa3ee4ddd
Ilya Siamionau <ilya.siamionau@cycode.com>
2024-07-15 12:45:01 +0200

    CM-38164 - Fix extension loading when core is not found

--- src/extension/Cycode.VisualStudio.Extension.Shared/Helpers/HashHelper.cs
+++ src/extension/Cycode.VisualStudio.Extension.Shared/Helpers/HashHelper.cs
@@ -6,6 +6,10 @@ namespace Cycode.VisualStudio.Extension.Shared.Helpers;
 
 public static class HashHelper {
     public static async Task<bool> VerifyFileChecksumAsync(string filePath, string expectedChecksum) {
+        if (!File.Exists(filePath)) {
+            return false;
+        }
+
         using FileStream stream = File.OpenRead(filePath);
         SHA256 sha256 = SHA256.Create();
         byte[] hash = await Task.Run(() => sha256.ComputeHash(stream));
