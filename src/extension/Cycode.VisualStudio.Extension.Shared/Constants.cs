effcccdd8f348f99fc1eadac7cda2b4502083467
Ilya Siamionau <ilya.siamionau@cycode.com>
2024-07-25 12:17:16 +0200

    bump cli version

--- src/extension/Cycode.VisualStudio.Extension.Shared/Constants.cs
+++ src/extension/Cycode.VisualStudio.Extension.Shared/Constants.cs
@@ -7,7 +7,7 @@ public static class Constants {
 
     public static readonly string PluginPath = GetPluginsPath();
     public static readonly string DefaultCliPath = GetDefaultCliPath();
-    public const string RequiredCliVersion = "1.10.4";
+    public const string RequiredCliVersion = "1.10.7";
 
     public const string CycodeDomain = "cycode.com";
 
